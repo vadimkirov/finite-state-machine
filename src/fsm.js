a2aab6d917db0736114a3b0d15b1d9201157f3dc
vadimkirov <vadimkirov@yandex.ru>
2019-10-09 13:48:49 +0300

    Один вопрос к redo

--- src/fsm.js
+++ src/fsm.js
@@ -3,6 +3,9 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
+
+
+
     constructor(config) {
         if (config == null) {
             throw new Error('config isn\'t passed');
@@ -10,6 +13,8 @@ class FSM {
             this.config = config;
         }
         this.config.state = this.config.initial;
+        this.undoStack = [];
+        this.redoStack = [];
     }
 
     /**
@@ -27,10 +32,29 @@ class FSM {
     changeState(state) {
 
         if(state in  this.config.states){
+
+            if(this.undoStack.length === 0 && state !== this.config.initial.toString()){
+
+                this.undoStack.push(this.getState().toString());
+            }
             this.config.state = state;
+            this.undoStack.push(this.config.state);
         }else {
             throw new Error('state isn\'t exist');
         }
+
+
+        // if(state in  this.config.states){
+        //
+        //     if(this.undoStack.length === 0 && state !== this.config.initial.toString()){
+        //
+        //         this.undoStack.push(this.getState().toString());
+        //     }
+        //     this.config.state = state;
+        //     this.undoStack.push(this.config.state);
+        // }else {
+        //     throw new Error('state isn\'t exist');
+        // }
 
     }
 
@@ -50,7 +74,8 @@ class FSM {
             if (event !== key) {
                 continue;
             }
-            this.changeState(newState[key]);
+             this.changeState(newState[key]);
+           // this.undoStack.push(this.changeState(newState[key]));
             checkEvent = false;
         }
 
@@ -109,7 +134,24 @@ class FSM {
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(this.undoStack.length === 0){
+            return false;
+        }else {
+
+            this.redoStack.push(this.undoStack.pop());
+
+            if(this.undoStack.length === 0){
+                return false;
+            }
+
+
+            this.changeState(this.undoStack.pop());
+
+            return true;
+        }
+
+    }
 
     /**
      * Goes redo to state.
@@ -117,6 +159,18 @@ class FSM {
      * @returns {Boolean}
      */
     redo() {
+        if(this.redoStack.length === 0){
+            return false;
+        }else {
+
+            if(this.getState().toString() === this.redoStack[this.redoStack.length -1]){
+                return false;
+            }
+            let redoState = this.redoStack.pop();
+            this.undoStack.push(redoState);
+            this.changeState(redoState);
+            return true;
+        }
 
     }
 
@@ -124,48 +178,52 @@ class FSM {
      * Clears transition history
      */
     clearHistory() {
-
+        this.redoStack.length = 0;
+        this.undoStack.length = 0;
     }
 }
 
-// module.exports = FSM;
-const config = {
-    initial: 'normal',
-    states: {
-        normal: {
-            transitions: {
-                study: 'busy',
-            }
-        },
-        busy: {
-            transitions: {
-                get_tired: 'sleeping',
-                get_hungry: 'hungry',
-            }
-        },
-        hungry: {
-            transitions: {
-                eat: 'normal'
-            },
-        },
-        sleeping: {
-            transitions: {
-                get_hungry: 'hungry',
-                get_up: 'normal',
-            },
-        },
-    }
-};
-
-
-const student = new FSM(config);
-
-
-
-student.trigger('study');
-
- student.trigger('eat');
-student.trigger('get_up');
-
-let cccc= 'hjhkljh';
+module.exports = FSM;
+// const config = {
+//     initial: 'normal',
+//     states: {
+//         normal: {
+//             transitions: {
+//                 study: 'busy',
+//             }
+//         },
+//         busy: {
+//             transitions: {
+//                 get_tired: 'sleeping',
+//                 get_hungry: 'hungry',
+//             }
+//         },
+//         hungry: {
+//             transitions: {
+//                 eat: 'normal'
+//             },
+//         },
+//         sleeping: {
+//             transitions: {
+//                 get_hungry: 'hungry',
+//                 get_up: 'normal',
+//             },
+//         },
+//     }
+// };
+//
+//
+// const student = new FSM(config);
+//
+//
+// student.changeState('hungry');
+// student.undo();
+// student.changeState('normal');
+// student.undo();
+// student.changeState('busy');
+// student.redo();
+// let testing = student.redo();
+//
+//
+// let cccc= 'hjhkljh';
 /** @Created by Uladzimir Halushka **/
